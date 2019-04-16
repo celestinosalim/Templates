ffa3a8b283911a082027e570858f01b3ea831bef
celestinosalim <celestino.salim@gmail.com>
2019-04-16 15:17:48 -0400

    changing exports

--- src/Template.js
+++ src/Template.js
@@ -2,4 +2,4 @@ import React, { Component } from "react";
 import Profile from "./components/Profile";
 import About from "./components/About";
 
-export default { Profile, About };
+export { Profile, About };
