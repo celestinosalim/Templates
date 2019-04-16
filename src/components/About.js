ab320b9a2fae265a26ee198a2e8fcfa548820585
celestinosalim <celestino.salim@gmail.com>
2019-04-16 17:26:57 -0400

    testing css on template

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>Hello from About</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div style={{ fontSize: "30px" }}>
+            <h1>Hello fucking world</h1>
+          </div>
+        </div>
+        <div className="col">
+          <div style={{ fontSize: "20px", color: "red" }}>
+            <h1>Second, but last</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
