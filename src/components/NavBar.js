63f3a0d4495abb9ec1a34d8f758c0721d7d3438f
celestinosalim <celestino.salim@gmail.com>
2019-04-17 00:46:06 -0400

    added proptypes to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,74 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {
+  navLinks: PropTypes.array.isRequired,
+  styles: PropTypes.object,
+  menu: PropTypes.string.isRequired
+};
+
+const defaultProps = {
+  navLinks: [
+    {
+      pathName: "FirstLink",
+      pathRef: "/"
+    },
+    {
+      pathName: "SecondLink",
+      pathRef: "/second"
+    },
+    {
+      pathName: "ThirdLink",
+      pathRef: "/third"
+    }
+  ],
+  styles: {
+    back: {
+      backgroundColor: "white"
+    },
+    menuColor: {
+      backgroundColor: "",
+      color: "black"
+    },
+    links: {
+      color: "black"
+    }
+  },
+  name: "MENU"
+};
 
 class NavBar extends Component {
   render() {
+    const styles = this.props.styles || {};
     return (
       <div className="container-fluid">
-        <nav class="navbar bg-transparent navbar-fixed-top">
+        <nav
+          className="navbar bg-transparent navbar-fixed-top"
+          style={{
+            position: "fixed"
+          }}
+        >
           <ul data-toggle="collapse">
-            <h1
+            <h3
               data-toggle="collapse"
               href="#collapseExample"
               role="button"
               aria-expanded="false"
               aria-controls="collapseExample"
+              style={styles.menuColor}
             >
-              MENU
-            </h1>
-            <div class="collapse" id="collapseExample">
-              <div class="card card-body">
-                <ul class="nav-item active">
+              {this.props.name}
+            </h3>
+            <div className="collapse" id="collapseExample">
+              <div>
+                <ul className="nav-item active">
                   {this.props.navLinks.map(nav => {
                     return (
-                      <a class="nav-link" href={nav.pathRef}>
+                      <a
+                        style={styles.links}
+                        className="nav-link"
+                        href={nav.pathRef}
+                      >
                         {nav.pathName}
                       </a>
                     );
@@ -35,21 +83,7 @@ class NavBar extends Component {
   }
 }
 
-NavBar.defaultProps = {
-  navLinks: [
-    {
-      pathName: "FirstLink",
-      pathRef: "/"
-    },
-    {
-      pathName: "SecondLink",
-      pathRef: "/second"
-    },
-    {
-      pathName: "ThirdLink",
-      pathRef: "/third"
-    }
-  ]
-};
+NavBar.propTypes = propTypes;
+NavBar.defaultProps = defaultProps;
 
 export default NavBar;
