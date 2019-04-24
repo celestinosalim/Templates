90ec79668e4e97ee1a88090d0ca0cfd84280d6a9
celestinosalim <celestino.salim@gmail.com>
2019-04-24 15:37:07 -0400

    Fixed About component Layout, added Css added default props

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/About.css";
 
-const About = () => {
-  return (
-    <div className="container">
-      <div className="photo">
-        <img
-          src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/019tha_ons_crd_02.jpg"
-          alt=""
-        />
-        <div class="glow-wrap">
-          <i class="glow" />
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  imageURL: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+const defaultProps = {
+  name: "Anonymous Name",
+  imageURL:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrDKzB0PCK5FMOwzf0rwE97xlRUhgOQsg4l0nwLkyK4NYXzQJ_",
+  description: `It is a long established fact that a reader will be distracted
+  by the readable content of a page when looking at its layout.
+  The point of using Lorem Ipsum is that it has a more-or-less
+  normal distribution of letters, as opposed to using 'Content
+  here, content here', making it look like readable English. Many
+  desktop publishing packages and web page editors now use Lorem
+  Ipsum as their default model text, and a search for 'lorem
+  ipsum' will uncover many web sites still in their infancy.
+  Various versions have evolved over the years, sometimes by
+  accident, sometimes on purpose (injected humour and the like).`
+};
+
+class About extends Component {
+  render() {
+    return (
+      <div className="container about-container">
+        <div className="row first-row">
+          <div className="col">
+            <div className="title">
+              <span>{this.props.name.toUpperCase()}</span>
+            </div>
+          </div>
+        </div>
+        <div className="row second-row">
+          <div className="col-md-6 col-sm-12 image-col">
+            <img
+              alt="About Image"
+              className="my-card"
+              src={this.props.imageURL}
+            />
+            <div className="glow-wrap">
+              <i className="glow" />
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12 text-col">
+            <span>ABOUT</span>
+            <p>____</p>
+
+            <div className="text-col-content">
+              <p>{this.props.description}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+About.propTypes = propTypes;
+About.defaultProps = defaultProps;
 
 export default About;
