97d5ed6ea28731f22e0af1c25d0ea94b94e2727b
celestinosalim <celestino.salim@gmail.com>
2019-04-30 15:45:01 -0400

    Forgot to imported and exported on Templated that JS fixed

--- src/Template.js
+++ src/Template.js
@@ -1,5 +1,6 @@
 import Profile from "./components/Profile";
 import About from "./components/About";
 import SideBar from "./components/SideBar";
+import Intro from "./components/intro/Intro";
 
-export { Profile, About, SideBar };
+export { Profile, About, SideBar, Intro };
