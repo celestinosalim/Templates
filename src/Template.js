063a56bb0813e5264a5497765a0384494b49f572
celestinosalim <celestino.salim@gmail.com>
2019-05-03 17:52:46 -0400

    Added import and export to template.js

--- src/Template.js
+++ src/Template.js
@@ -2,5 +2,7 @@ import Profile from "./components/Profile";
 import About from "./components/About";
 import SideBar from "./components/SideBar";
 import Intro from "./components/intro/Intro";
+import ProjectContainer from "./components/projects/ProjectContainer";
+import ProjectCard from "./components/projects/ProjectCard";
 
-export { Profile, About, SideBar, Intro };
+export { Profile, About, SideBar, Intro, ProjectContainer, ProjectCard };
