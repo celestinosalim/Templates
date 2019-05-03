0ee563c56f0540d0698bef945d41dc0f38561caa
celestinosalim <celestino.salim@gmail.com>
2019-05-03 18:00:49 -0400

    Fixed import and export error projectcontainer

--- src/components/projects/ProjectContainer.js
+++ src/components/projects/ProjectContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ProjectCard from "./ProjectCard";
-import "./ProjectContainer.css";
+import "../../styles/ProjectContainer.css";
 
 const propTypes = {
   projects: PropTypes.array.isRequired
