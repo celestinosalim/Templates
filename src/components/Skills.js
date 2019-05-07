7266871f13dd19a12141fe58e78942caf4683374
celestinosalim <celestino.salim@gmail.com>
2019-05-06 23:21:29 -0400

    Added Skills && Skills.css

---
+++ src/components/Skills.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/Skills.css";
+
+const arrayOfLength = function(expectedLength, props, propName, componentName) {
+  const arrayPropLength = props[propName].length;
+  let err;
+
+  if (arrayPropLength > expectedLength) {
+    return new Error(
+      `Invalid array length ${arrayPropLength} (expected ${expectedLength}) for prop ${propName} supplied to ${componentName}. Validation failed.`
+    );
+  }
+};
+
+const propTypes = {
+  skills: arrayOfLength.bind(0, 14)
+};
+
+const defaultProps = {
+  skills: [
+    "Ruby",
+    "Python",
+    "Javascript",
+    "Ruby on Rails",
+    "React",
+    "Redux",
+    "Django"
+  ]
+};
+
+const skillsToRender = skills => {
+  return skills.map((skill, key) => (
+    <div className="box" key={key++}>
+      <div className="boxContent">
+        <ul className="title">{skill}</ul>
+      </div>
+    </div>
+  ));
+};
+
+const Skills = props => {
+  let { skills } = props;
+
+  return (
+    <div className="container justify-content-center">
+      <ul className="skillsContainer">{this.skillsToRender(skills)}</ul>;
+    </div>
+  );
+};
+
+Skills.propTypes = propTypes;
+Skills.defaultProps = defaultProps;
+
+export default Skills;
